d55b96ba9ab298faafb7fa4e201fff3922b8854b
Andrew Huntington <andrew.huntington@gmail.com>
2019-11-19 20:39:19 +0900

    added a basic sfx for when moving the cursor

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,14 +7,17 @@ public class MenuController : MonoBehaviour
 {
     private int cursorPosition = 0;
     [SerializeField] GameObject[] CursorObjects;
+    [Tooltip("Sound when moving the cursor")] [SerializeField] AudioClip blipSFX;
     GameObject currentCursorState;
     GameObject previousCursorState;
     Vector2 grabInputValue;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         currentCursorState = CursorObjects[cursorPosition];
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,8 @@ public class MenuController : MonoBehaviour
         grabInputValue.y = Input.GetAxisRaw("Vertical");
         if (Input.GetButtonDown("Vertical"))
         {
+            audioSource.PlayOneShot(blipSFX);
+
             if (grabInputValue.y < 0 && cursorPosition < CursorObjects.Length - 1)
             {
                 previousCursorState = CursorObjects[cursorPosition];
